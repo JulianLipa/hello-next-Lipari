fb856f3a8c2e5b001e87a14c1da84b0fcfcf683b
JulianLipa <lipajulianp@gmail.com>
2024-05-16 20:20:26 -0300

    fetch indio

--- src/app/components/fetchData.js
+++ src/app/components/fetchData.js
@@ -14,7 +14,7 @@ const FetchData = () => {
       const handleFetchData = async () => {
         try {
           const response = await axios.get(
-            "https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=The+Strokes&api_key=d659cc02b91f8569a8d3a3e95091db43&format=json"
+            "https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=Patricio+Rey&api_key=d659cc02b91f8569a8d3a3e95091db43&format=json"
           );
           const data = response.data.topalbums.album;
           setAlbums(data);
